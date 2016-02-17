ff72e35eaba3b973645caca65537ebe4858d8ff5
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-02-17 12:05:49 -0600

    Blocked. Getting assistance from instructors.

--- converter.js
+++ converter.js
@@ -1,15 +1,18 @@
-console.log("hello");
-
-function toCelsius () {
-
+function toCelsius() {
+  var inputTemp = document.getElementById("inputField").value;
+  var outputTemp = (((inputTemp - 32) * 5) / 9);
+  document.getElementById("output").innerHTML = "<p>" + outputTemp + "</p>";
 }
 
-function toFahrenheit () {
-
+function toFahrenheit() {
+  var inputTemp = document.getElementById("inputField").value;
+  var outputTemp = (((inputTemp * 9) / 5) + 32);
+  document.getElementById("output").innerHTML = "<p>" + outputTemp + "</p>";
 }
 
 // Get a reference to the button element in the DOM
 var button = document.getElementById("converterId");
+button.addEventListener("click", toCelsius() );
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
@@ -19,5 +22,8 @@ function determineConverter (clickEvent) {
 
 // Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
+
+// This is code for stlying converted temperature from green to blue or red.
+// 
 
 

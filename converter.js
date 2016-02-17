2ba3c30d8844b14ec84bb213f69ba215125cb95e
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-02-17 17:36:33 -0600

    Got the conversion working. Color changing with .style method still needed.

--- converter.js
+++ converter.js
@@ -12,16 +12,27 @@ function toFahrenheit() {
 
 // Get a reference to the button element in the DOM
 var button = document.getElementById("converterId");
-button.addEventListener("click", toCelsius() );
+button.addEventListener("click", determineConverter);
+
+var clearButton = document.getElementById("clear");
+
+// Take this to function 
+// var removedText = document.getElementById("inputField").value =
+//   document.getElementById("inputField").value ;
+clearButton.addEventListener("click", removedText);
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
-function determineConverter (clickEvent) {
-  console.log("event", clickEvent);
+
+function determineConverter() {
+  if (document.getElementById("toC").checked) {
+    toCelsius();
+  } else if (document.getElementById("toF").checked) {
+  toFahrenheit();
+  }
 }
 
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+  // console.log("event", clickEvent);
 
 // This is code for stlying converted temperature from green to blue or red.
 // 
